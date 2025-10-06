d34bfb491167ebcf93c64470157a8615e7762b2d
Jeremy Lu <zhillu@tesla.com>
2025-10-06 16:20:01 -0700

    installing code mirror

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import { Theme, Button, Flex, Heading } from '@radix-ui/themes';
+import { Theme, Flex, Heading } from '@radix-ui/themes';
 import styled from '@emotion/styled';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import { useState } from 'react';
 
 function App() {
+  const [code, setCode] = useState("console.log('Hello, Flow Writer!');");
+
   return (
     <Theme appearance="dark">
-      <Flex direction="column" align="center" justify="center" gap="4" style={{ minHeight: '100vh' }}>
+      <Flex direction="column" align="center" gap="4" style={{ minHeight: '100vh', padding: '40px' }}>
+        <div style={{ width: '100%', maxWidth: '800px' }}>
+          <CodeMirror
+            value={code}
+            height="400px"
+            theme="dark"
+            extensions={[javascript()]}
+            onChange={(value) => setCode(value)}
+          />
+        </div>
       </Flex>
     </Theme>
   );
